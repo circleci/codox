3e55842d72f854cdc99d36278173057cf0566aea
Karoline Pauls <code@karolinepauls.com>
2021-08-28 16:48:58 +0100

    Bump deps causing breakage

--- codox/project.clj
+++ codox/project.clj
@@ -4,9 +4,9 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [org.clojure/clojurescript "1.7.189"]
+                 [org.clojure/clojurescript "1.10.866"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.6"]
                  [org.pegdown/pegdown "1.6.0"]
--- example/project.clj
+++ example/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :plugins [[lein-codox "0.10.7"]]
   :source-paths ["src/clojure"]
   :target-path "target/%s/"
